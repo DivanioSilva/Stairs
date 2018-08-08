3cea5146b7b5eeead5fbc4996d5a42279a351e83
Divanio Silva <divanio.silva@focus-bc.com>
2018-08-08 15:53:49 +0100

    Instalados bootstrap e material design. Adicionados dois botões no app.components.html. Não é a melhor forma de  fazer, mas pro que é chega!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
