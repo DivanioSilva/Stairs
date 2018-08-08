4bfefc0e1406bc7ed5ba5ebc96b29387cadb5ecb
Divanio Silva <divanio.silva@focus-bc.com>
2018-08-08 14:56:21 +0100

    again...

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -13,4 +12,4 @@ res.sendFile(path.join(__dirname,'/dist/home/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
